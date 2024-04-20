7f2d73dc112446f13b156db0eb1eda78f5cd06c5
Bunyod <89516132+drbunyod@users.noreply.github.com>
2024-04-20 18:43:12 +0500

    Refactor using ES6 classes

--- index.js
+++ index.js
@@ -1,111 +1,117 @@
-function Library() {
-    this.books = [];
+class Library {
+    constructor() {
+        this.books = [];
+    }
+
+    getBooks() {
+        return this.books;
+    }
+
+    countBooks() {
+        return this.books.length;
+    }
+
+    addBook(book) {
+        this.books.push(book);
+    }
+
+    deleteBook(id) {
+        this.books.splice(id, 1);
+    }
 }
 
-Library.prototype.getBooks = function() {
-    return this.books;
-};
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 
-Library.prototype.countBooks = function() {
-    return this.books.length;
-};
+    getReadStatus() {
+        return this.read ? "Read" : "Not read";
+    }
 
-Library.prototype.addBook = function(book) {
-    this.books.push(book);
-};
-
-Library.prototype.deleteBook = function(id) {
-    this.books.splice(id, 1);
-};
-
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+    toggleReadStatus() {
+        this.read = !this.read;
+    }
 }
 
-Book.prototype.getReadStatus = function() {
-    return this.read ? "Read" : "Not read";
-};
+class ScreenController {
+    constructor() {
+        this.library = new Library();
 
-Book.prototype.toggleReadStatus = function() {
-    this.read = !this.read;
-};
-
-function ScreenController() {
-    this.library = new Library();
-
-    const form = document.querySelector("form");
-    const newBookTitle = document.querySelector("#title");
-    const newBookAuthor = document.querySelector("#author");
-    const newBookPages = document.querySelector("#pages");
-    const newBookRead = document.querySelector("#read");
-
-    form.addEventListener("submit", e => {
-        e.preventDefault();
+        const form = document.querySelector("form");
+        const newBookTitle = document.querySelector("#title");
+        const newBookAuthor = document.querySelector("#author");
+        const newBookPages = document.querySelector("#pages");
+        const newBookRead = document.querySelector("#read");
     
-        if (newBookTitle.value == "") {
-            alert("Title must be entered.");
-        } else if (newBookAuthor.value == "") {
-            alert("Author must be entered.");
-        } else if (newBookPages.value == "") {
-            alert("Number of pages must be entered.");
+        form.addEventListener("submit", e => {
+            e.preventDefault();
+        
+            if (newBookTitle.value == "") {
+                alert("Title must be entered.");
+            } else if (newBookAuthor.value == "") {
+                alert("Author must be entered.");
+            } else if (newBookPages.value == "") {
+                alert("Number of pages must be entered.");
+            } else {
+                this.library.addBook(new Book(newBookTitle.value, newBookAuthor.value, newBookPages.value, newBookRead.checked));
+                e.target.reset();
+                this.updateDisplay();
+            }
+        });
+    
+        this.table = document.querySelector("tbody");
+    
+        this.updateDisplay();
+    }
+
+    updateDisplay() {
+        this.table.innerHTML = "";
+    
+        if (this.library.countBooks() === 0) {
+            this.table.innerHTML = `<tr><td class="not-found" colspan="6">No books found</td></tr>`;
         } else {
-            this.library.addBook(new Book(newBookTitle.value, newBookAuthor.value, newBookPages.value, newBookRead.checked));
-            e.target.reset();
-            this.updateDisplay();
-        }
-    });
-
-    this.table = document.querySelector("tbody");
-
-    this.updateDisplay();
-}
-
-ScreenController.prototype.updateDisplay = function() {
-    this.table.innerHTML = "";
-
-    if (this.library.countBooks() === 0) {
-        this.table.innerHTML = `<tr><td class="not-found" colspan="6">No books found</td></tr>`;
-    } else {
-        const books = this.library.getBooks();
-        for (let i = 0; i < this.library.countBooks(); i++) {
-            this.table.innerHTML += `
-            <tr>
-                <td>${i+1}</td>
-                <td>${books[i].title}</td>
-                <td>${books[i].author}</td>
-                <td>${books[i].pages}</td>
-                <td class="table-status">
-                    <button type="button" class="read-status-btn" data-id="${i}">${books[i].getReadStatus()}</button>
-                </td>
-                <td class="table-delete">
-                    <button type="button" class="delete-btn" data-id="${i}">Delete</button>
-                </td>
-            </tr>`;
-        }
-
-        const readStatusBtns = document.querySelectorAll(".read-status-btn");
-        for (const btn of readStatusBtns) {
-            btn.addEventListener("click", this.changeReadStatus.bind(this));
-        }
-
-        const deleteBtns = document.querySelectorAll(".delete-btn");
-        for (const btn of deleteBtns) {
-            btn.addEventListener("click", this.deleteBook.bind(this));
+            const books = this.library.getBooks();
+            for (let i = 0; i < this.library.countBooks(); i++) {
+                this.table.innerHTML += `
+                <tr>
+                    <td>${i+1}</td>
+                    <td>${books[i].title}</td>
+                    <td>${books[i].author}</td>
+                    <td>${books[i].pages}</td>
+                    <td class="table-status">
+                        <button type="button" class="read-status-btn" data-id="${i}">${books[i].getReadStatus()}</button>
+                    </td>
+                    <td class="table-delete">
+                        <button type="button" class="delete-btn" data-id="${i}">Delete</button>
+                    </td>
+                </tr>`;
+            }
+    
+            const readStatusBtns = document.querySelectorAll(".read-status-btn");
+            for (const btn of readStatusBtns) {
+                btn.addEventListener("click", this.changeReadStatus.bind(this));
+            }
+    
+            const deleteBtns = document.querySelectorAll(".delete-btn");
+            for (const btn of deleteBtns) {
+                btn.addEventListener("click", this.deleteBook.bind(this));
+            }
         }
     }
-};
- 
-ScreenController.prototype.changeReadStatus = function(e) {
-    this.library.getBooks()[e.target.dataset.id].toggleReadStatus();
-    this.updateDisplay();
-};
 
-ScreenController.prototype.deleteBook = function(e) {
-    this.library.deleteBook(e.target.dataset.id);
-    this.updateDisplay();
-};
+    changeReadStatus(e) {
+        this.library.getBooks()[e.target.dataset.id].toggleReadStatus();
+        this.updateDisplay();
+    }
+    
+    deleteBook(e) {
+        this.library.deleteBook(e.target.dataset.id);
+        this.updateDisplay();
+    }
+}
 
 const screenController = new ScreenController();
